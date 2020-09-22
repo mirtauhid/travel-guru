3a718fad58d27e2a0b2681d252b6840f1a12261f
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-09-23 01:08:59 +0600

    starting travel-guru project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,43 +9,53 @@ import Booking from './Components/Booking/Booking';
 import Contact from './Components/Contact/Contact';
 import Destination from './Components/Destination/Destination';
 import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
+import Login from './Components/Authentication/Login/Login';
 import News from './Components/News/News';
 import NotFound from './Components/NotFound/NotFound';
-import SignUp from './Components/SignUp/SignUp';
+import SignUp from './Components/Authentication/SignUp/SignUp';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+
+
+export const UserContext = createContext();
+
+
 const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/news">
-          <News />
-        </Route>
-        <Route path="/destination">
-          <Destination />
-        </Route>
-        <Route path="/blog">
-          <Blog />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/booking">
-          <Booking />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/destination">
+            <Destination />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/booking">
+            <Booking />
+          </PrivateRoute>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h3>This is a Home</h3>
+            <h3>This is Home</h3>
         </div>
     );
 };
